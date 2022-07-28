60e04c7d0dd650c681da7ead81e8769f85aca37a
Jouella Fabe <jojofabe@gmail.com>
2022-07-28 10:24:40 -0400

    add compile options here
    Signed-off-by: Jouella Fabe <jojofabe@gmail.com>

--- setup.rb
+++ setup.rb
@@ -1,10 +1,17 @@
-Dir.mkdir 'binary_data'
+Dir.mkdir 'binary_data' unless Dir.exist?('binary_data')
+options = RubyVM::InstructionSequence.compile_option
+options[:inline_const_cache] = false
+options[:peephole_optimization] = false
+options[:specialized_instruction] = false
+options[:operands_unification] = false
+options[:coverage_enabled] = false
+
 Dir.foreach('code') do |file|
   next unless file.end_with?('.rb')
   filename = file.match(/(task[0-9]).rb/i).captures.first
 
-  file = File.open("binary_data/#{filename}", 'wb')
-  file.write(RubyVM::InstructionSequence.compile_file(file).to_binary)
+  f = File.open("binary_data/#{filename}", 'wb')
+  f.write(RubyVM::InstructionSequence.compile_file("code/#{file}", **options).to_binary)
 
-  file.close
+  f.close
 end
